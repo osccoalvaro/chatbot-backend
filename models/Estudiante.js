91c18c5b4cc0e05ce1ab64fa398e6558973d098a
osccoalvaro <osccoalvaro@gmail.com>
2025-05-10 02:25:20 -0500

    update ok2

--- models/Estudiante.js
+++ models/Estudiante.js
@@ -6,7 +6,9 @@ const estudianteSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   apellidoPaterno: { type: String, required: true },
   apellidoMaterno: { type: String, required: true },
+  tipoAdmision: { type: String, required: true },
   grado: { type: String, required: true },
+  condicionMedica: { type: String, required: true }, 
   imagen: { type: mongoose.Schema.Types.ObjectId, required: true }, // ID de GridFS
   imagenLibreta: { type: mongoose.Schema.Types.ObjectId, required: true }, // Imagen de la Libreta en GridFS
   estadoAdmision: { type: String, enum: ["Pendiente", "Admitido", "Observado"], default: "Pendiente" },
